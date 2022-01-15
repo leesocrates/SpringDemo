6159bdf1a3d1f18cef98c5f5e00c92265d4f2983
loverice <2038517202@qq.com>
2022-01-15 18:20:31 +0800

    修改apollo配置

--- src/main/java/com/lee/demo/MainApplication.java
+++ src/main/java/com/lee/demo/MainApplication.java
@@ -1,10 +1,15 @@
 package com.lee.demo;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableApolloConfig
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
+        System.setProperty("env","DEV");
+        System.setProperty("apollo.configService", "http://106.14.1.219:8100");
         SpringApplication.run(MainApplication.class, args);
     }
 }
--- src/main/java/com/lee/demo/controller/MainController.java
+++ src/main/java/com/lee/demo/controller/MainController.java
@@ -1,15 +1,19 @@
 package com.lee.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class MainController {
+
+    @Value(value = "${hello: 默认名字}")
+    private String helloStr;
 
     @ResponseBody
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello World!";
+        return "Hello World!"+helloStr;
     }
 
 }
